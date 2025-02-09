c25b3df76128b5216778d2bce5de38da97ef1e15
gouga10 <a.abderrazek10@gmail.com>
2025-02-09 12:39:45 +0400

    organized and created dockerfiles

---
+++ UI/streamlit.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import requests
+def get_last_4_messages(conversation):
+    # Take the last 4 messages (if available)
+    last_msgs = conversation[-6:]
+
+
+    
+    return last_msgs
+# Title of the app
+st.title("Medical Records Chatbot")
+
+# Initialize session state to store chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Hello i am your medical records assistant , how can i help you ?"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Send user message to local API and get response
+    api_url = "http://localhost:8001/generate"
+    payload = {"query": prompt,'conv':get_last_4_messages(st.session_state.messages)}
+    response = requests.post(api_url, json=payload)
+
+    if response.status_code == 200:
+        bot_response = response.json()["response"]
+    else:
+        bot_response = "Sorry, there was an error processing your request."
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(bot_response)
+    
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": bot_response})
